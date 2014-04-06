1f5f396e01c8c9a13a5a6695c926780a0905b69c
kelvinau <kelvinau4413@hotmail.com>
2014-04-06 01:44:35 -0700

    Changed animation of the accordion slider

--- accordion.js
+++ accordion.js
@@ -3,20 +3,19 @@ var accordion_page = 1;
 function slide(to){
 	if (to != accordion_page){
 		$("#accordion"+(accordion_page*5-4)).animate({width:'toggle'},800);
-		$('#accordion'+(to*5-4)).show();
-		
+		$('#accordion'+(to*5-4)).show(800);
+
 		$("#accordion"+(accordion_page*5-3)).animate({width:'toggle'},800);
-		$('#accordion'+(to*5-3)).show();
-		
+		$('#accordion'+(to*5-3)).show(800);
+
 		$("#accordion"+(accordion_page*5-2)).animate({width:'toggle'},800);
-		$('#accordion'+(to*5-2)).show();
-		
+		$('#accordion'+(to*5-2)).show(800);
+
 		$("#accordion"+(accordion_page*5-1)).animate({width:'toggle'},800);
-		$('#accordion'+(to*5-1)).show();
+		$('#accordion'+(to*5-1)).show(800);
 
 		$("#accordion"+(accordion_page*5)).animate({width:'toggle'},800);
-		$('#accordion'+(to*5)).show();
+		$('#accordion'+(to*5)).show(800);
 		accordion_page = to;
 	}
-	
 }
